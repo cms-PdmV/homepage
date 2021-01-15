8b320077108079381b7c4febd7919fe3a5f0a037
Justinas Rumsevicius <justinas.rumsevicius@gmail.com>
2021-01-15 14:30:37 +0100

    Add plot sharing

--- src/components/plot/PdmVPlot.vue
+++ src/components/plot/PdmVPlot.vue
@@ -22,14 +22,35 @@
       <v-col xs="12" sm="12" md="12" lg="4" cols="12">
         <div class="pa-2">
           <PdmVPlotFilter
+            ref='filterPanel'
             :fetchedData="fetchedData"
             :eventBus="bus">
           </PdmVPlotFilter>
           <v-btn small class="primary mt-2" @click="downloadPlot">Save as PNG</v-btn>
+          <v-btn small class="primary mt-2 ml-2" @click="sharePlot">Share</v-btn>
         </div>
       </v-col>
     </v-row>
     <small style="opacity: 0.4">For Monte Carlo production, only MiniAOD and NanoAOD campaigns are shown</small>
+    <v-dialog v-if="shareURL" v-model="shareURL" width="640">
+      <v-card class="pt-2 pl-2 pr-2">
+        <h2 class="ml-2 mr-2">Share Event Production plot</h2>
+        <div class="ml-2 mr-2">
+          <b>URL:</b> <small><a :href="shareURL">{{shareURL}}</a></small>
+          <br>
+          <b>Please note:</b> sharing this URL will share selected filters and options, but does not include plot data (no data snapshot).
+          Plot in PdmV Homepage is updated multiple times per day, so it is likely that using this URL in the future will
+          yield a different histogram - some campaigns/blocks/PWGs might not be present anymore. Time range "last X days/weeks/months"
+          will have a different meaning in the future as well.
+          <br>
+          If you want to share this exact plot, use "Save as PNG" button to download an image.
+        </div>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn small class="ma-1" color="primary" @click="shareURL = undefined">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -51,10 +72,8 @@ export default {
       bus: new Vue(),
       fetchedData: {},
       plotData: {'summary': {}},
+      shareURL: undefined,
     }
-  },
-  created () {
-    this.fetchData();
   },
   mounted () {
     let component = this
@@ -64,11 +83,6 @@ export default {
     this.bus.$on('timeRangeChange', function (timeRange) {
       component.fetchData(timeRange);
     })
-  },
-  props: {
-  },
-  watch: {
-
   },
   components: {
     PdmVBarChart,
@@ -86,6 +100,7 @@ export default {
       });
     },
     prepareData(filters) {
+      this.dataFilters = filters;
       var newData = {}
       var monteCarloSum = 0;
       var rerecoSum = 0;
@@ -168,12 +183,41 @@ export default {
                    .replace('.80', '.8')
                    .replace('.90', '.9')
     },
-    handleFilterChange(filters) {
-      this.dataFilters = filters;
-      this.prepareData(this.dataFilters)
-    },
     downloadPlot() {
       this.bus.$emit('downloadPlot');
+    },
+    sharePlot() {
+      let filters = this.$refs.filterPanel.getAllValues();
+      let query = [];
+      if (filters.timeRange != 'week') {
+        query.push('time_range=' + filters.timeRange);
+      }
+      if (filters.plotMode != 'cumulative') {
+        query.push('plot_mode=' + filters.plotMode);
+      }
+      if (filters.plotScale != 'linear') {
+        query.push('plot_scale=' + filters.plotScale);
+      }
+      if (filters.bubbleLegend != 'off') {
+        query.push('bubble_legend=' + filters.bubbleLegend);
+      }
+      if (filters.pwgs.filter(p => !p.selected).length) {
+        query.push('pwgs=' + filters.pwgs.filter(p => p.selected).map(p => p.name).join(','));
+      }
+      if (filters.blocks.filter(b => !b.selected).length) {
+        query.push('blocks=' + filters.blocks.filter(b => b.selected).map(b => b.number).join(','));
+      }
+      let isNano = function(name) {
+        return name.toLowerCase().indexOf('nano') != -1;
+      }
+      if (filters.campaigns.filter(c => (c.selected && isNano(c.name)) || (!c.selected && !isNano(c.name))).length) {
+        query.push('campaigns=' + filters.campaigns.filter(c => c.selected).map(c => c.name).join(','));
+      }
+      let url = window.location.origin + window.location.pathname;
+      if (query.length) {
+        url += '?' + query.join('&');
+      }
+      this.shareURL = url;
     }
   }
 }
